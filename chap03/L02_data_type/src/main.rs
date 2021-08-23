eec3bbf42bb60b0f22cc0ec87c4bad0dce81c997
zhangcheng <472012214@qq.com>
2021-08-24 01:02:50 +0800

    chap 03 02=data type

---
+++ chap03/L02_data_type/src/main.rs
@@ -0,0 +1,57 @@
+fn main() {
+    //error: type annotations needed
+    // let guess = "42".parse().expect("Not a number!");
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of number is: {}", guess);
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // 加法
+    let sum = 5 + 10;
+
+    // 减法
+    let difference = 95.5 - 4.3;
+    // 乘法
+    let product = 4 * 30;
+    // 除法
+    let quotient = 56.7 / 32.2;
+    // 取余
+    let remainder = 43 % 5;
+
+    let t = true;
+    let f: bool = false; // 显式指定类型注解
+
+    //char
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    //tup
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+    println!("The value of one is: {}", one);
+
+
+    //array
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+    println!("The value of first is: {}", first);
+
+    //error: index out of bounds
+    // let a = [1, 2, 3, 4, 5];
+    // let index = 10;
+    // let element = a[index];
+    // println!("The value of element is: {}", element);
+
+}
