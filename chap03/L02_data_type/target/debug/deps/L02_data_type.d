1fc8654c9392b46589544292df9814601fd9b26c
zhangcheng <472012214@qq.com>
2021-09-07 15:08:13 +0800

    delete target

--- chap03/L02_data_type/target/debug/deps/L02_data_type.d
+++
@@ -1,5 +0,0 @@
-/Users/bixin/Documents/projects/rust-language-learning/chap03/L02_data_type/target/debug/deps/L02_data_type: src/main.rs
-
-/Users/bixin/Documents/projects/rust-language-learning/chap03/L02_data_type/target/debug/deps/L02_data_type.d: src/main.rs
-
-src/main.rs:
